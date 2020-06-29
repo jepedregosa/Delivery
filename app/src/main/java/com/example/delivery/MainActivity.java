e60c8a81d168e3010c18e9e4022864729ec97e30
jercas1 <51945537+jercas1@users.noreply.github.com>
2020-06-29 15:27:19 +0800

    Added Login Toast

--- app/src/main/java/com/example/delivery/MainActivity.java
+++ app/src/main/java/com/example/delivery/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.delivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String TAG = "MainActivity";
     EditText client_code, company, user, password;
     Button login_btn;
 
@@ -22,8 +25,25 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         login_btn = findViewById(R.id.login_btn);
 
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
 
+    private void login() {
+        String client_code_text = client_code.getText().toString();
+        String company_text = company.getText().toString();
+        String user_text = user.getText().toString();
+        String password_text = password.getText().toString();
 
+        if (client_code_text.matches("") && company_text.matches("")
+            && user_text.matches("") && password_text.matches("")) {
+            Toast.makeText(getApplicationContext(), "All credentials must be filled.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
