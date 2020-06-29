b44d5396227d294e420d47970b939054e57de120
jercas1 <51945537+jercas1@users.noreply.github.com>
2020-06-29 17:33:48 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/delivery/MainActivity.java
+++ app/src/main/java/com/example/delivery/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedpreferences = getSharedPreferences(BuildConfig.APPLICATION_ID + ".credentials", Context.MODE_PRIVATE);
+
         client_code = findViewById(R.id.client_code);
         company = findViewById(R.id.company);
         user = findViewById(R.id.user);
@@ -32,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    SharedPreferences sharedpreferences;
+
     private void login() {
         String client_code_text = client_code.getText().toString();
         String company_text = company.getText().toString();
@@ -43,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "All credentials must be filled.", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_LONG).show();
+
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putString("client_code", client_code_text);
+            editor.putString("company", company_text);
+            editor.putString("user", user_text);
+            editor.putString("password", password_text);
+            editor.commit();
         }
 
     }
